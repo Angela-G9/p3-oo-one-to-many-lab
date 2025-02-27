07e1795e2fc0e0c645ac11d0daa23c7937d49e16
Angela-G9 <angelagathoni9568@gmail.com>
2025-02-27 15:05:51 +0300

    labdone

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in self.PET_TYPES:
+            raise ValueError(f"Invalid pet type: {pet_type}. Must be one of {self.PET_TYPES}")
+        self.pet_type = pet_type
+        self.owner = owner
+        self.__class__.all.append(self)
+
+        if owner is not None:
+            owner.add_pet(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Argument must be an instance of Pet")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
